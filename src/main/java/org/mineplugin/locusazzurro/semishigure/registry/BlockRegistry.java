5726eb1e31524f331a9e59f0d91b1e522e7cd0cb
LocusAzzurro <dingzx1997@gmail.com>
2023-09-08 21:35:26 +0200

    :sparkles: opera figure for teacon use

--- src/main/java/org/mineplugin/locusazzurro/semishigure/registry/BlockRegistry.java
+++ src/main/java/org/mineplugin/locusazzurro/semishigure/registry/BlockRegistry.java
@@ -1,6 +1,7 @@
 package org.mineplugin.locusazzurro.semishigure.registry;
 
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
@@ -11,4 +12,6 @@ public class BlockRegistry {
 
     public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, Semishigure.MOD_ID);
     public static final RegistryObject<Block> TAIKO_BLOCK = BLOCKS.register("taiko_block", TaikoBlock::new);
+
+    public static final RegistryObject<Block> OPERA_FIGURE = BLOCKS.register("opera_figure", () -> new Block(BlockBehaviour.Properties.of().noOcclusion().noCollission().noParticlesOnBreak().noLootTable()));
 }
